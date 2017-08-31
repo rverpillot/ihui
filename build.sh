a844e10ede678e039269d2a3a78d1e0a35eec012
regis <rverpi90@gmail.com>
2017-08-31 17:05:38 +0200

    .

--- build.sh
+++ build.sh
@@ -4,4 +4,3 @@ go get github.com/GeertJohan/go.rice
 go get github.com/PuerkitoBio/goquery
 go get github.com/gorilla/websocket
 go build -a -x -o bin/example src/rverpi/ihui.v2/example/main.go src/rverpi/ihui.v2/example/menu.go
-bin/example
