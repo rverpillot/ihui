e5334ec9cc64c9f5ab3e4591977be11f09af8752
regis <rverpi90@gmail.com>
2017-08-31 16:17:48 +0200

    .

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 mkdir -p src/rverpi/ihui.v2 && mv example *.go *.js vendor ihui_resources src/rverpi/ihui.v2/
 ls -R /var/app
+export GOPATH=$(pwd)
 go build -x -o bin/example src/rverpi/ihui.v2/example/main.go src/rverpi/ihui.v2/example/menu.go
 exit $?
