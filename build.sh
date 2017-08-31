ccc683bec6b4817812f5154275b6e1ca3a8cc38b
regis <rverpi90@gmail.com>
2017-08-31 14:23:42 +0200

    .

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-go build -x -o bin/example example/main.go example/menu.go
+mkdir rverpi/ihui.v2 && mv example *.go *.js node_modules vendor ihui_resources rverpi/ihui.v2/
+go build -x -o bin/example rverpi/ihui.v2/example/main.go rverpi/ihui.v2/example/menu.go
