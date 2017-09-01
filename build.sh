b1b0757cd32cb193775453275067234663f20861
regis <rverpi90@gmail.com>
2017-09-01 09:45:00 +0200

    .

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 mkdir -p src/rverpi/ihui.v2 && mv example *.go *.js vendor ihui_resources src/rverpi/ihui.v2/
 export GOPATH=$(pwd)
+APP=$(pwd)
 go get github.com/GeertJohan/go.rice
 go get github.com/GeertJohan/go.rice/rice
 go get github.com/PuerkitoBio/goquery
 go get github.com/gorilla/websocket
 go build -a -x -o bin/example src/rverpi/ihui.v2/example/main.go src/rverpi/ihui.v2/example/menu.go
-(cd src/rverpi/ihui.v2 && $GOPATH/bin/rice -v append --exec bin/example)
+(cd src/rverpi/ihui.v2 && $GOPATH/bin/rice -v append --exec $APP/bin/example)
